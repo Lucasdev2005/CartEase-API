1b6078f342ec7c3c30c39662bf269ca0cec252fc
Lucas Moreira <lucasmoreira@etssoft.com.br>
2023-10-18 09:29:41 -0300

    Update user.repository.ts

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -3,6 +3,6 @@ import { User } from "@prisma/client";
 
 export default class UserRepository extends baseRepository<User> {
 
-    constructor(public user: User) {super("user")}
+    constructor() {super("user")}
 
 }
