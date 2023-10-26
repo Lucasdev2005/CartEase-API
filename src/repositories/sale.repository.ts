8b930153b47eb4c33de337e1cfb04f75cfa666df
Lucas Moreira Nunes <lucasdev2005@gmail.com>
2023-10-25 22:10:23 -0300

    códigho para salvar uma venda.

--- src/repositories/sale.repository.ts
+++ src/repositories/sale.repository.ts
@@ -1,22 +1,54 @@
 import { baseRepository } from "./base.repository";
 import { Product, Sale } from "@prisma/client";
 import ProductRepository from "./product.repository";
+import { HttpException, HttpStatus } from "@nestjs/common";
+import ProductSaleRepository from "./productSale.repository";
 
 export default class SaleRepository extends baseRepository<Sale> {
     constructor() {super("sale")}
 
     public async createItem(data: any): Promise<Sale> {
         const productRepository = new ProductRepository();
+        const productSaleRepository = new ProductSaleRepository();
+        let SaleValue: number = 0; 
+
+        //verify, products have stock
         for (const product of data.products) {
             let productFound: Product = await  productRepository.findItem({
-                PRD_ProductId: product.PRS_ProductId
+                PRD_ProductId: product.PRD_ProductId
             });
-            if (productFound.PRD_stock < (productFound.PRD_stock - product.PRS_Quantity)) {
-                
+            SaleValue += productFound.PRD_Price * product.PRS_Quantity;
+            if ((productFound.PRD_stock - product.PRS_Quantity) < 0) {
+                throw new HttpException('Product dont have Stock', HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
-        data.SLE_DateSale = await new Date(data.SLE_DateSale);
-        let saleSaved = super.createItem(data);
 
+        //save a sale
+        let saleSaved: Sale = await super.createItem({
+            ...data.sale,
+            SLE_Value: SaleValue,
+        });
+
+        //save sale Products
+        for (const product of data.products) {
+            let productFound: Product = await  productRepository.findItem({
+                PRD_ProductId: product.PRD_ProductId
+            });
+            productSaleRepository.createItem({
+                PRS_SaleId: saleSaved.SLE_SaleId,
+                PRS_ProductId: product.PRD_ProductId,
+                PRS_Quantity: product.PRS_Quantity
+            });
+            productRepository.updateItem({
+                data: {
+                    PRD_stock: productFound.PRD_stock - product.PRS_Quantity
+                },
+                where: {
+                    PRD_ProductId: product.PRD_ProductId
+                }
+            })
+        }
+
+        return saleSaved;
     }
 }
