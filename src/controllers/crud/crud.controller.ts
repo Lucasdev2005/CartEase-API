ccef838c21bc57dfc1973b4ba9d3c73f7b6ec28b
Lucas Moreira Nunes <lucasdev2005@gmail.com>
2023-10-29 20:13:22 -0300

    alterando visibilidade de função e nomenclatura.

--- src/controllers/crud/crud.controller.ts
+++ src/controllers/crud/crud.controller.ts
@@ -6,17 +6,17 @@ export class CrudController {
 
     @Get('getResource')
     public async getResource(@Headers() headers, @Body() body) {
-        return await this.getRepository(headers.repository).findItem(body.where);
+        return await this._getRepository(headers.repository).findItem(body.where);
     }
 
     @Post('createResource')
     public async createResource(@Headers() headers, @Body() body) {
-        return await this.getRepository(headers.repository).createItem(body);
+        return await this._getRepository(headers.repository).createItem(body);
     }
 
     @Put('updateResource')
     public async updateResource(@Headers() headers, @Body() body) {
-        return await this.getRepository(headers.repository).updateItem({
+        return await this._getRepository(headers.repository).updateItem({
             data: body.data,
             where: body.where
         });
@@ -24,12 +24,12 @@ export class CrudController {
 
     @Delete("deleteResource")
     public async deleteResource(@Headers() headers, @Body() body) {
-        return await this.getRepository(headers.repository).deleteItem(body.where);
+        return await this._getRepository(headers.repository).deleteItem(body.where);
     }
 
     @Get("ListResource")
     public async listResource(@Headers() headers, @Body() body) {
-        return this.getRepository(headers.repository).findAllItemsBy({
+        return this._getRepository(headers.repository).findAllItemsBy({
             distinct: body.distinct || {},
             select: body.select || {},
             orderBy: body.orderBy || {},
@@ -37,7 +37,7 @@ export class CrudController {
         });
     }
 
-    public getRepository(repository): baseRepository<any> {
+    private _getRepository(repository): baseRepository<any> {
         let repositoryClass = require(`../../repositories/${repository}.repository`);
         try {
             let repositoryInstanced = new repositoryClass.default();
