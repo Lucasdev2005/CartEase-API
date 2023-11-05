bf97ba444b6255d565762d1cc8e9b6196b3622a4
Lucas Moreira Nunes <lucasdev2005@gmail.com>
2023-11-05 20:53:57 -0300

    import inutilizado.

--- src/controllers/crud/crud.controller.ts
+++ src/controllers/crud/crud.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Head, Headers, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Headers, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { baseRepository } from 'src/repositories/base.repository';
 import { AuthGuard } from '../auth/auth.guard';
 
