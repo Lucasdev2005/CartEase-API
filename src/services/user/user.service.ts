2b2109ab73439b5801d09449e71e6cb069a38b83
Lucas Moreira Nunes <lucas2005dev@gmail.com>
2023-12-01 09:00:57 -0300

    Hash senha de usários.

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { CrudBaseService } from '../CrudBase.service';
 import { User } from '../../database/entities/User';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService extends CrudBaseService<User> {
+    
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>
     ) {
         super(userRepository)
+    }
+
+    public async createResource(data: DeepPartial<User>): Promise<User> {
+        data.USR_UserPassword = await bcrypt.hash(data.USR_UserPassword, 10);
+        return await super.createResource(data);
     }
 }
